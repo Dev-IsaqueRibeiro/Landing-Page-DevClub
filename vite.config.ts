78eb4144d8d570c4a47b5498c94ef0232b138933
Dev-IsaqueRibeiro <dev.isaqueribeiro@gmail.com>
2026-04-06 18:55:40 -0300

    feat: Setup Inicial

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // Essencial para GitHub Pages e Firebase
+  base: "./",
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"), // Para o Shadcn/UI funcionar
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
